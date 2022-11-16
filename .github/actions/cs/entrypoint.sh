febc6d1a68c506e04d9b93bcd89f20b2439d6488
France <miles.france@gmail.com>
2022-11-16 20:08:10 +0000

    phpcs.yml: remove showing level since it is secret and do not show in the log

--- .github/actions/cs/entrypoint.sh
+++ .github/actions/cs/entrypoint.sh
@@ -3,10 +3,9 @@
 # Fail on error
 set -e
 
-cd drupal_repo
 export PATH="$PATH:/root/drupal/vendor/bin"
+cd drupal_repo
 echo "--------- CS O'Clock -------------"
 phpcs --standard=Drupal,DrupalPractice ./
 echo "--------- PHPSTAN O'Clock -------------"
-echo "Running phpstan at level $phpstan_level"
 phpstan analyse -l $phpstan_level ./
